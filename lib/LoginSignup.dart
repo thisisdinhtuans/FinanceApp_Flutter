19ce8cf0ac6b1b0af294b24a3e14072eaeb6721e
Dinh Tuans <dinhmanhtuans89@gmail.com>
2024-01-16 22:44:10 +0700

    first commit

---
+++ lib/LoginSignup.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+
+class LoginSignup extends StatefulWidget {
+  const LoginSignup({super.key});
+
+  @override
+  State<LoginSignup> createState() => _LoginSignupState();
+}
+
+TextEditingController usernameController = TextEditingController();
+TextEditingController passwordController = TextEditingController();
+
+void login(String username, password) async {
+  var client = http.Client();
+  try {
+    var url = Uri.http('10.0.2.2:5296', '/api/account/login');
+    var headers = {'Content-Type': 'application/json'};
+    var body = convert.jsonEncode({
+      'username': username,
+      'password': password,
+    });
+
+    var response = await client.post(url, headers: headers, body: body);
+
+    if (response.statusCode == 200) {
+      print('Success');
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  } catch (error) {
+    print('Error: $error');
+  } finally {
+    client.close();
+  }
+}
+// try {
+//   Response response =
+//       await post(Uri.parse('10.0.2.2:5296/api/account/login'), body: {
+//     'username': username,
+//     'password': password,
+//   });
+//   if (response.statusCode == 200) {
+//     print('account created');
+//   } else {
+//     print('failed');
+//   }
+// } catch (e) {
+//   print(e.toString());
+// }
+// }
+
+class _LoginSignupState extends State<LoginSignup> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TextField(
+            controller: usernameController,
+            decoration: InputDecoration(
+                hintText: 'username', border: OutlineInputBorder()),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          TextField(
+            controller: passwordController,
+            decoration: InputDecoration(
+                hintText: 'Password', border: OutlineInputBorder()),
+          ),
+          const SizedBox(height: 20),
+          GestureDetector(
+            onTap: () {
+              login(usernameController.text.toString(),
+                  passwordController.text.toString());
+            },
+            child: Container(
+              height: 60,
+              width: 300,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: const Center(
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    ));
+  }
+}
